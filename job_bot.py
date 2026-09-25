c46ee4c55f446af1cfb70c8ea49a9588cbd9ca90
lilfarisar3-sudo <lil.faris.a.r.3@gmail.com>
2026-09-25 16:42:26 +0300

    Fix main guard syntax and token formatting

--- job_bot.py
+++ job_bot.py
@@ -102,7 +102,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         context.user_data["awaiting_input"] = None
         await update.message.reply_text(f"✅ تم العثور على الدولة وإضافتها: {text}")
 
-if __name__ == "__main__"8887237109:AAEFRaH5SUwgGbOytQW456ynnlz3uQ3hJKY"
+if __name__ == "__main__"8887237109:8887237109:"AAEFRaH5SUwgGbOytQW456ynnlz3uQ3hJKY"
 
     app = ApplicationBuilder().token("8887237109:AAEvWP5C_Xou9UluRoJgkfBMrGjoJBD9ExA").build()
 
